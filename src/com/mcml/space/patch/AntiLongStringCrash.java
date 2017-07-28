7df658ce886050c3ee2350cf8223ee5d821c88df
Vlvxingze <1207223090@qq.com>
2017-07-28 15:54:19 +0800

    修复一个刷错BUG，新增一个机制

--- src/com/mcml/space/patch/AntiLongStringCrash.java
+++ src/com/mcml/space/patch/AntiLongStringCrash.java
@@ -1,8 +1,10 @@
 package com.mcml.space.patch;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -13,12 +15,32 @@ public class AntiLongStringCrash implements Listener{
 	
 	@SuppressWarnings("deprecation")
 	@EventHandler
-	public void ClickCheckItem(PlayerInteractEvent event){
+	public void InteractCheck(PlayerInteractEvent event){
 		if(ConfigPatch.AntiLongStringCrashenable == true){
 			ItemStack item = event.getItem();
 			Player player = event.getPlayer();
 			if(item != null){
-				if(item.hasItemMeta()){
+				if(item.hasItemMeta() && item.getItemMeta().getDisplayName() != null){
+					if(item.getItemMeta().getDisplayName().length() > 20){
+						player.setItemInHand(null);
+						AzureAPI.log(player, ConfigPatch.AntiLongStringCrashWarnMessage);
+					}
+				}
+			}
+		}
+	}
+	
+	@SuppressWarnings("deprecation")
+	@EventHandler
+	public void ClickCheckItem(InventoryClickEvent event){
+		if(ConfigPatch.AntiLongStringCrashenable == true){
+			if(event.getWhoClicked().getType() != EntityType.PLAYER){
+				return;
+			}
+			Player player = (Player) event.getWhoClicked();
+			ItemStack item = event.getCursor();
+			if(item != null){
+				if(item.hasItemMeta() && item.getItemMeta().getDisplayName() != null){
 					if(item.getItemMeta().getDisplayName().length() > 20){
 						player.setItemInHand(null);
 						AzureAPI.log(player, ConfigPatch.AntiLongStringCrashWarnMessage);
