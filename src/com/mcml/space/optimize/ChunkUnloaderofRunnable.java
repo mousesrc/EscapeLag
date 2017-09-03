f5994207b5ce65782317f600aa1378c4612c4981
Administrator <Administrator@PC-20170309XDJU>
2017-09-03 22:27:25 +0800

    导致内存溢出的分段ChunkUnloader？？？

--- src/com/mcml/space/optimize/ChunkUnloaderofRunnable.java
+++ src/com/mcml/space/optimize/ChunkUnloaderofRunnable.java
@@ -1,42 +1,36 @@
 package com.mcml.space.optimize;
 
-import java.util.Iterator;
 import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.World;
 
 import com.mcml.space.config.ConfigOptimize;
-import com.mcml.space.core.EscapeLag;
 
 public class ChunkUnloaderofRunnable implements Runnable {
-    public static int ChunkUnloaderTimes;
+	public static int ChunkUnloaderTimes;
 
-    @Override
-    public void run() {
-        if (ConfigOptimize.chunkUnloader == true) {
-            List<World> worlds = Bukkit.getWorlds();
-            final Iterator<World> it = worlds.iterator();
-            int dotick = 0;
-            while(it.hasNext()) {
-            	dotick = dotick + 5;
-            	Bukkit.getScheduler().runTaskLater(EscapeLag.MainThis, new Runnable() {
-            		public void run() {
-            			World world = it.next();
-                        Chunk[] loadedChunks = world.getLoadedChunks();
-                        int lcl = loadedChunks.length;
-                        for(int ii=0;ii<lcl;ii++){
-                            Chunk chunk = loadedChunks[ii];
-                            if(world.isChunkInUse(chunk.getX(),chunk.getZ())==false){
-                                if(chunk.isLoaded() == true & ChunkKeeper.ShouldKeepList.contains(chunk)==false){
-                                    chunk.unload();
-                                    ChunkUnloaderTimes++;
-                                }
-                            }
-                        }
-            		}
-            	}, dotick);
-            }
-        }
-    }
+	@Override
+	public void run() {
+		if (ConfigOptimize.chunkUnloader == true) {
+			if (ConfigOptimize.chunkUnloader == true) {
+	            List<World> worlds = Bukkit.getWorlds();
+	            int ws = worlds.size();
+	            for (int i = 0; i < ws; i++) {
+	                World world = worlds.get(i);
+	                Chunk[] loadedChunks = world.getLoadedChunks();
+	                int lcl = loadedChunks.length;
+	                for(int ii=0;ii<lcl;ii++){
+	                    Chunk chunk = loadedChunks[ii];
+	                    if(world.isChunkInUse(chunk.getX(),chunk.getZ())==false){
+	                        if(chunk.isLoaded() == true & ChunkKeeper.ShouldKeepList.contains(chunk)==false){
+	                            chunk.unload();
+	                            ChunkUnloaderTimes++;
+	                        }
+	                    }
+	                }
+	            }
+	        }
+		}
+	}
 }
